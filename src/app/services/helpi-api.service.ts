b8f9eae34c55f895eff706eee6aaf0efec05712a
JoseRoncal2002 <u201912909@upc.edu.pe>
2021-06-19 20:40:27 -0500

    api-conection

--- src/app/services/helpi-api.service.ts
+++ src/app/services/helpi-api.service.ts
@@ -1,20 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root'
+})
+
 export class HelpiApiService {
-  // Students Endpoint
-  //basePath = 'https://help-i-api.azurewebsites.net/api';
+
   constructor(private http: HttpClient) { }
-  // HTTP Default Options
-
-// API Error Handling
-
 
   // Get Game by Id
   getGameById(id: number): any {
     return this.http.get(`https://help-i-api.azurewebsites.net/api/games/${id}`);
   }
+
   // Get Game Data
   getAllGames():any{
     return this.http.get(`https://help-i-api.azurewebsites.net/api/games`);
